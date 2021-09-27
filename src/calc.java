4a7dedb5fed2dc3c91ceaa6ba538abd732ec8cf3
minah <s215722@student.dtu.dk>
2021-09-27 14:43:27 +0200

    * com

--- src/calc.java
+++ src/calc.java
@@ -13,7 +13,7 @@ public class calculator2 {
             System.out.print("\nEnter an operator (+, -, *, /): ");
             op = reader.next().charAt(0);
             switch(op) {
-                case '+': ans = num1 + num2;//a1
+                case '+': ans = num1 * num2;//a1
                     break;
                 case '-': ans = num1 - num2;
                     break;
@@ -26,7 +26,7 @@ public class calculator2 {
             }
             System.out.print("\nThe result is given as follows:\n");
             System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
-            System.out.println("system")
+            System.out.println("system");
         }
 
 }
